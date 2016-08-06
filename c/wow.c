f22123210915255b44b62a11dc73e539ca53483d
hrj <harshad.rj@gmail.com>
2016-08-06 23:52:32 +0530

    first cut

---
+++ c/wow.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void groupStart(char *id) {
+  printf("<g id='%s'>", id);
+}
+
+void groupEnd() {
+  printf("</g>");
+}
+
+void rect() {
+  printf("<rect width='100' height='100' x='10' y='10' style='fill:maroon;' />");
+}
+
+void svgStart(int width, int height) {
+  printf("<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n\
+<svg xmlns:svg='http://www.w3.org/2000/svg' \
+   xmlns='http://www.w3.org/2000/svg' \
+   version='1.1'                       \
+   width='%d'                     \
+   height='%d' > \
+   ", width, height);
+}
+
+void svgEnd() {
+  printf("</svg>");
+}
+
+int main(void) {
+  svgStart(600, 600);
+  groupStart("layer1");
+  rect();
+  groupEnd();
+  svgEnd();
+  return 0;
+}
