e911f09b14f1fa422e1250d033000e3d1fd6020e
hrj <harshad.rj@gmail.com>
2016-08-07 00:32:26 +0530

    Added scala version

---
+++ scala/wow.scala
@@ -0,0 +1,25 @@
+import scala.xml.Node
+
+def svg(width: Int, height: Int, n:Node) = {
+  <svg xmlns:svg="http://www.w3.org/2000/svg"  xmlns="http://www.w3.org/2000/svg"  version="1.1" width={""+width} height={""+height} >
+   {n}
+  </svg>
+}
+
+def group(n:Node) = {
+  <g id="layer1">{n}</g>
+}
+
+def rect() = {
+  <rect width="100" height="100" x="10" y="10" style="fill:maroon;" />
+}
+
+println("""<?xml version='1.0' encoding='UTF-8' standalone='no'?>""")
+
+println(
+  svg(600, 600,
+    group(
+      rect()
+    )
+  )
+)
